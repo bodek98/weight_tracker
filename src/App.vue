3134c5b8f8ca4a1faa25e67c7d35adc67841b50b
bodek98 <bodzio.entire@gmail.com>
2022-10-22 16:16:59 +0200

    New project with compoonents and some styles, early version

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+<WeightTracker/>
+  
+</template>
+
+<script>
+import WeightTracker from './components/WeightTracker/WeightTracker.vue'
+
+export default {
+  name: 'App',
+  components: {
+    WeightTracker
+  }
+}
+</script>
+
+<style>
+body {
+  margin: 0;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-size: 10px;
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+  background-color: #2c3e50;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin: 0;
+  padding: 0;
+  /* border: solid; */
+}
+</style>
---
+++ src/components/WeightTracker/WeightTracker.vue
@@ -0,0 +1,13 @@
+<template>
+  <header class="header">
+ Welcome to WeightTracker
+  </header>
+  <div class="container">
+
+  </div>
+</template>
+
+<script src="./index.js"></script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style src="./style.css" scoped></style>
