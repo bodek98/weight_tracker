487a5ecaf4eacd72cab7751369a88c906d35065d
bodek98 <bodzio.entire@gmail.com>
2022-10-23 21:51:27 +0200

    Added GraphSection and connect to show/hide with grandchild component of FactsSection

---
+++ src/components/graphSection/GraphSection.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="graph">
+      <h2 class="graph__h2">Weight Graph</h2>
+
+    </div>
+  </template>
+  
+  <script>
+  
+  export default {
+    components: {
+    },
+    props: {
+      msg: String,
+    },
+    data() {
+      return {
+        
+      }
+    },
+    methods: {
+
+    }
+  };
+  </script>
+  
+  <!-- Add "scoped" attribute to limit CSS to this component only -->
+  <style src="./GraphSection.css" scoped></style>
+  
