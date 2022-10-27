e50b805d38a0ffde01718601c34cea5d5c34c733
bodek98 <bodzio.entire@gmail.com>
2022-10-27 21:47:28 +0200

    Hide button update, little styles

--- src/components/factsSection/FactsTile/FactsTile.js
+++ src/components/factsSection/FactsTile/FactsTile.js
@@ -1,16 +1,22 @@
 export default {
-    props: {
-      title: String,
-      bgColor: String,
-      fColor: String,
-      content: Number,
-      buttonSubmit: Boolean,
-      hidden: Boolean
+  props: {
+    title: String,
+    bgColor: String,
+    fColor: String,
+    content: Number,
+    buttonSubmit: Boolean,
+    hidden: Boolean,
+  },
+  data() {
+    return {
+      isHiddenLoc: true,
+    };
+  },
+  methods: {
+    showGraph() {
+      this.isHidden = !this.isHidden;
+      this.isHiddenLoc = !this.isHidden;
+      this.$emit("clickedShowGraph", this.isHidden);
     },
-    methods: {
-      showGraph() {
-        this.isHidden = !this.isHidden;
-        this.$emit('clickedShowGraph', this.isHidden);
-      }
-    }
-  };
+  },
+};
--- src/components/factsSection/FactsTile/FactsTile.vue
+++ src/components/factsSection/FactsTile/FactsTile.vue
@@ -2,12 +2,8 @@
   <div class="facts_box" :style="{ backgroundColor: bgColor, color: fColor }">
     <h3 v-if="title != null" class="facts_box__h3">{{ title }}</h3>
     <p v-if="content != null" class="facts_box__p">{{ content }}</p>
-    <button
-      class="facts_box__button"
-      v-if="buttonSubmit === true"
-      @click="showGraph"
-    >
-      Show
+    <button v-if="buttonSubmit" class="facts_box__button" @click="showGraph">
+      {{ isHiddenLoc ? "Show" : "Hide" }}
     </button>
   </div>
 </template>
